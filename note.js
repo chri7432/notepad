0b4971cd26dc1e39d99e73bdb569d033c35937ea
Christian Bang <christianbang@Christians-MacBook-Pro-2.local>
2018-03-12 12:51:42 +0100

    fghj

--- note.js
+++ note.js
@@ -7,3 +7,12 @@
   var getName = prompt('Your name?');
   localStorage.setItem('name', getName);
   }
+
+  <script>
+  function myFunction() {
+      var para = document.createElement("P");
+      var t = document.createTextNode("This is a paragraph.");
+      para.appendChild(t);
+      document.getElementById("myDIV").appendChild(para);
+  }
+  </script>
