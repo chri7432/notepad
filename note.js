de6ce3a2e5eae51701a2481a3bd79355f00a5e12
Christian Bang <christianbang@Christians-MacBook-Pro-2.local>
2018-03-12 10:24:56 +0100

    honey

--- note.js
+++ note.js
@@ -2,7 +2,7 @@
 
   var name = localStorage.getItem('name');
   if(name != 'null') {
-  alert('Hello ' + name);
+  alert('Hello ' + My Honey);
   } else {
   var getName = prompt('Your name?');
   localStorage.setItem('name', getName);
